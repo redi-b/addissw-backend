a403778889964b4218ef3d5515c76769f774d73d
rediet <rb.rediet@gmail.com>
2025-07-21 00:02:32 +0300

    add cors origin

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,9 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL
+}));
 app.use(express.json());
 
 app.use("/api/songs", songRouter);
