795121e87e28a006df1b8a21b756717cc02204a5
rediet <rb.rediet@gmail.com>
2025-08-01 12:12:10 +0300

    improve validation error message

--- src/validators/userValidator.ts
+++ src/validators/userValidator.ts
@@ -2,6 +2,12 @@ import { z } from "zod";
 
 // Schema for creating a user
 export const UserSchema = z.object({
-  username: z.string().min(4).max(20),
-  password: z.string().min(6).max(50),
+  username: z
+    .string()
+    .min(4, "Username must be at least 4 characters long")
+    .max(20, "Username must be at most 20 characters long"),
+  password: z
+    .string()
+    .min(6, "Password must be at least 6 characters long")
+    .max(50, "Password must be at most 50 characters long"),
 });
