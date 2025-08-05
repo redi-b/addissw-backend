5edb775e03969024cdc5105591d53a72cc140b27
rediet <rb.rediet@gmail.com>
2025-08-05 10:10:53 +0300

    remove unused migration files for songs and users

--- prisma/migrations/20250718160612_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Song" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "title" TEXT NOT NULL,
-    "artist" TEXT NOT NULL,
-    "album" TEXT,
-    "year" INTEGER,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
--- prisma/migrations/20250721164502_add_song_unique_constraint/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[title,artist]` on the table `Song` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Song_title_artist_key" ON "Song"("title", "artist");
